626d2a151624b291816b583957b412c9d3853b93
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2022-12-20 15:39:06 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+      name='labelboost',
+      version='0.0.1',
+      author='Labelbox',
+      author_email='raphael@labelbox.com',
+      description='Labelbox Helper Library',      
+      packages=setuptools.find_packages(),
+      url="https://labelbox.com",
+      long_description=long_description,
+      long_description_content_type="text/markdown",
+      install_requires=["labelbox", "packaging"],
+      keywords=["labelbox", "labelboost"],
+      extras_require={'dev': ['pylint']}
+)
