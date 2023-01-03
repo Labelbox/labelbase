3c8c72bef39816bc10919aceefd2bf0b55de6cfe
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-03 16:13:23 -0500

    Update connector.py

--- labelbase/connector.py
+++ labelbase/connector.py
@@ -1,16 +1,4 @@
 from labelbox import Client as labelboxClient
-from google.api_core import retry
-  
-@retry.Retry(predicate=retry.if_exception_type(Exception), deadline=240.)
-def upload_local_file(lb_client:labelboxClient, file_path:str):
-    """ Wraps client.upload_file() in retry logic
-    Args:
-        lb_client   :   Required (labelbox.client.Cient) - Labelbox Client object
-        file_path   :   Required (str) - Data row file path
-    Returns:
-        URL corresponding to the uploaded asset
-    """ 
-    return lb_client.upload_file(file_path)    
 
 def check_global_keys(client:labelboxClient, global_keys:list):
     """ Checks if data rows exist for a set of global keys
