ebb1a724636646e63609c47a460ab605c0b2ca9c
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-20 13:04:56 -0500

    Update coco.py

--- labelbase/converters/coco.py
+++ labelbase/converters/coco.py
@@ -1,4 +1,4 @@
-from labelbox import Client
+from labelbase.converters.ontology import get_ontology_schema_to_name_path
 import copy
 import datetime
 from google.api_core import retry
@@ -11,7 +11,7 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from shapely.geometry import Polygon
 import cv2
 
-def to_coco_bbox_converter(data_row_id, annotation, category_id):
+def _to_coco_bbox_converter(data_row_id, annotation, category_id):
     """ Given a label dictionary and a bounding box annotation from said label, will return the coco-converted bounding box annotation dictionary
     Args:
         data_row_id (str)               :     Labelbox Data Row ID for this label
@@ -28,7 +28,7 @@ def to_coco_bbox_converter(data_row_id, annotation, category_id):
     }
     return coco_annotation
 
-def to_coco_line_converter(data_row_id, annotation, category_id):
+def _to_coco_line_converter(data_row_id, annotation, category_id):
     """ Given a label dictionary and a line annotation from said label, will return the coco-converted line annotation dictionary
     Args:
         data_row_id (str)               :     Labelbox Data Row ID for this label
@@ -54,7 +54,7 @@ def to_coco_line_converter(data_row_id, annotation, category_id):
     }
     return coco_annotation, num_line_keypoints
 
-def to_coco_point_converter(data_row_id, annotation, category_id):
+def _to_coco_point_converter(data_row_id, annotation, category_id):
     """ Given a label dictionary and a point annotation from said label, will return the coco-converted point annotation dictionary
     Args:
         data_row_id (str)               :     Labelbox Data Row ID for this label
@@ -72,7 +72,7 @@ def to_coco_point_converter(data_row_id, annotation, category_id):
     }
     return coco_annotation  
 
-def to_coco_polygon_converter(data_row_id, annotation, category_id):
+def _to_coco_polygon_converter(data_row_id, annotation, category_id):
     """Given a label dictionary and a point annotation from said label, will return the coco-converted polygon annotation dictionary
     Args:
         data_row_id (str)               :     Labelbox Data Row ID for this label
@@ -104,7 +104,7 @@ def download_mask(annotation):
     """ 
     return requests.get(annotation['instanceURI']).content 
 
-def to_coco_mask_converter(data_row_id, annotation, category_id):
+def _to_coco_mask_converter(data_row_id, annotation, category_id):
     """Given a label dictionary and a mask annotation from said label, will return the coco-converted segmentation mask annotation dictionary
     Args:
         data_row_id (str)               :     Labelbox Data Row ID for this label
@@ -130,148 +130,151 @@ def to_coco_mask_converter(data_row_id, annotation, category_id):
     }  
     return coco_annotation
 
-def to_coco_annotation_converter(data_row_id, annotation, ontology_index):
+def _to_coco_annotation_converter(data_row_id, annotation, ontology_schema_to_name_path):
     """ Wrapper to triage and multithread the coco annotation conversion - if nested classes exist, the category_id will be the first radio/checklist classification answer available
     Args:
-        data_row_id (str)               :     Labelbox Data Row ID for this label
-        annotation (dict)               :     Annotation dictionary from label['Label']['objects'], which comes from project.export_labels()
-        ontology_index (dict)           :     A dictionary where {key=featureSchemaId : value = {"encoded_value"} which corresponds to category_id
+        data_row_id (str)                   :     Labelbox Data Row ID for this label
+        annotation (dict)                   :     Annotation dictionary from label['Label']['objects'], which comes from project.export_labels()
+        ontology_schema_to_name_path (dict) :     A dictionary where {key=featureSchemaId : value = {"encoded_value"} which corresponds to category_id
     Returns:
         A dictionary corresponding to te coco annotation syntax - the category ID used will be the top-level tool 
     """
     max_line_keypoints = 0
-    category_id = ontology_index[annotation['schemaId']]['encoded_value']
+    category_id = ontology_schema_to_name_path[annotation['schemaId']]['encoded_value']
     if "classifications" in annotation.keys():
         if annotation['classifications']:
             for classification in annotation['classifications']:
                 if 'answer' in classification.keys():
                     if type(classification['answer']) == dict:
-                        category_id = ontology_index[classification['schemaId']]['encoded_value']
+                        category_id = ontology_schema_to_name_path[classification['schemaId']]['encoded_value']
                         break
                 else:
-                    category_id = ontology_index[classification['answers'][0]['schemaId']]['encoded_value']
+                    category_id = ontology_schema_to_name_path[classification['answers'][0]['schemaId']]['encoded_value']
                     break
     if "bbox" in annotation.keys():
-        coco_annotation = to_coco_bbox_converter(data_row_id, annotation, category_id)
+        coco_annotation = _to_coco_bbox_converter(data_row_id, annotation, category_id)
     elif "line" in annotation.keys():
-        coco_annotation, max_line_keypoints = to_coco_line_converter(data_row_id, annotation, category_id)
+        coco_annotation, max_line_keypoints = _to_coco_line_converter(data_row_id, annotation, category_id)
     elif "point" in annotation.keys():
-        coco_annotation = to_coco_point_converter(data_row_id, annotation, category_id)
+        coco_annotation = _to_coco_point_converter(data_row_id, annotation, category_id)
     elif "polygon" in annotation.keys():
-        coco_annotation = to_coco_polygon_converter(data_row_id, annotation, category_id)
+        coco_annotation = _to_coco_polygon_converter(data_row_id, annotation, category_id)
     else: 
-        coco_annotation = to_coco_mask_converter(data_row_id, annotation, category_id)     
+        coco_annotation = _to_coco_mask_converter(data_row_id, annotation, category_id)     
     return coco_annotation, max_line_keypoints
 
-def export_to_coco(project):
+def export_to_coco(project, verbose=True, divider="///"):
     """ Given a project and a list of labels, will create the COCO export json
     Args:
-        project (labelbox.schema.project.Project)   :   Labelbox project object
+        project     :   Required (labelbox.schema.project.Project) - Labelbox Project object
+        verbose     :   Optional (bool) - If True, prints information about code execution
+        divider     :   Optional (str) - String delineating the tool/classification/answer path for a given schema ID
     Returns:
-
+        Dicationary with 'info', 'licenses', 'images', 'annotations', and 'annotations' keys corresponding to a COCO dataset format
     """
+    if verbose:
+        print(f'Exporting labels from project ID {project.uid}')
     labels_list = project.export_labels(download=True)
-    # Info section generated from project information
+    if verbose:
+        print(f'Export complete - {len(labels_list)} labels to convert')    
     info = {
-        'description' : project.name,
-        'url' : f'https://app.labelbox.com/projects/{project.uid}/overview',
-        'version' : "1.0", 
-        'year' : datetime.datetime.now().year,
-        'contributor' : project.created_by().email,
-        'date_created' : datetime.datetime.now().strftime('%Y/%m/%d'),
+        'description' : project.name, 'url' : f'https://app.labelbox.com/projects/{project.uid}/overview', 'version' : "1.0", 
+        'year' : datetime.datetime.now().year, 'contributor' : project.created_by().email, 'date_created' : datetime.datetime.now().strftime('%Y/%m/%d'),
     }
-    # Licenses section is left empty
-    licenses = [
-        {
-            "url" : "N/A",
-            "id" : 1,
-            "name" : "N/A"
-        }
-    ]
+    licenses = [{"url" : "N/A","id" : 1,"name" : "N/A"}]
     # Create a dictionary where {key=data_row_id : value=data_row}
     data_rows = {}
-    print(f'Exporting Data Rows from Project...')
-    for dataset in project.datasets():
-        for data_row in dataset.export_data_rows():
-            data_rows.update({data_row.uid : data_row})  
-    print(f'\nExport complete. {len(data_rows)} Data Rows Exported')
-    # Images section generated from data row export
-    print(f'\nConverting Data Rows into a COCO Dataset...\n')
+    if verbose:
+        print(f'Exporting Data Rows from Project...')
+    if project.datasets():
+        for dataset in project.datasets():
+            for data_row in dataset.export_data_rows():
+                data_rows.update({data_row.uid : data_row})  
+    else:
+        for batch in project.batches():
+            for data_row in batch.export_data_rows():
+                data_rows.update({data_row.uid : data_row})  
+    if verbose:                        
+        print(f'Export complete. {len(data_rows)} Data Rows Exported')
+        print(f'Converting Data Rows into a COCO Dataset...')
     images = []
     data_row_check = [] # This is a check for projects where one data row has multiple labels (consensus, benchmark)
-    for label in tqdm(labels_list):
-        data_row = data_rows[label['DataRow ID']]
-        if label['DataRow ID'] not in data_row_check:
-            data_row_check.append(label['DataRow ID'])
-            images.append({
-                "license" : 1,
-                "file_name" : data_row.external_id,
-                "height" : data_row.media_attributes['height'],
-                "width" : data_row.media_attributes['width'],
-                "date_captured" : data_row.created_at.strftime('%Y-%m-%d %H:%M:%S'),
-                "id" : data_row.uid,
-                "coco_url": data_row.row_data
-            })
-    print(f'\nData Rows Converted into a COCO Dataset.')  
+    if verbose:
+        for label in tqdm(labels_list):
+            data_row = data_rows[label['DataRow ID']]
+            if label['DataRow ID'] not in data_row_check:
+                data_row_check.append(label['DataRow ID'])
+                images.append({
+                    "license" : 1, "file_name" : data_row.external_id, "height" : data_row.media_attributes['height'],
+                    "width" : data_row.media_attributes['width'], "date_captured" : data_row.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                    "id" : data_row.uid, "coco_url": data_row.row_data
+                })
+    else:
+        for label in labels_list:
+            data_row = data_rows[label['DataRow ID']]
+            if label['DataRow ID'] not in data_row_check:
+                data_row_check.append(label['DataRow ID'])
+                images.append({
+                    "license" : 1, "file_name" : data_row.external_id, "height" : data_row.media_attributes['height'],
+                    "width" : data_row.media_attributes['width'], "date_captured" : data_row.created_at.strftime('%Y-%m-%d %H:%M:%S'),
+                    "id" : data_row.uid, "coco_url": data_row.row_data
+                })  
+    if verbose:                     
+        print(f'Data Rows Converted into a COCO Dataset.')  
     annotations = []
-    print(f'\nConverting Annotations into the COCO Format...\n')
-    ontology_index = index_ontology(project.ontology().normalized) 
+    if verbose:                     
+        print(f'Converting Annotations into the COCO Format...\n')
+    ontology_schema_to_name_path = get_ontology_schema_to_name_path(project.ontology().normalized, detailed=True) 
     global_max_keypoints = 0
     futures = []
     with ThreadPoolExecutor() as exc:
         for label in tqdm(labels_list):
             for annotation in label['Label']['objects']:
-                futures.append(exc.submit(coco_annotation_converter, label['DataRow ID'], annotation, ontology_index))
+                futures.append(exc.submit(_to_coco_annotation_converter, label['DataRow ID'], annotation, ontology_schema_to_name_path))
         for f in as_completed(futures):
             res = f.result()
             if res[1] > global_max_keypoints:
                 global_max_keypoints = copy.deepcopy(res[1])
             annotations.append(res[0])    
-    print(f'\nAnnotation Conversion Complete. Converted {len(annotations)} annotations into the COCO Format.')    
-    categories = []
-    print(f'\nConverting the Ontology into the COCO Dataset Format...') 
-    for featureSchemaId in ontology_index:
-        if ontology_index[featureSchemaId]["type"] == "line": 
+    if verbose:                     
+        print(f'Annotation Conversion Complete. Converted {len(annotations)} annotations into the COCO Format.')                        
+        print(f'Converting the Ontology into the COCO Dataset Format...') 
+    categories = []        
+    for featureSchemaId in ontology_schema_to_name_path:
+        if ontology_schema_to_name_path[featureSchemaId]["type"] == "line": 
             keypoints = []
             skeleton = []
             for i in range(0, global_max_keypoints): 
                 keypoints.append(str("line_")+str(i+1))
                 skeleton.append([i, i+1])
             categories.append({
-                "supercategory" : ontology_index[featureSchemaId]['name'],
-                "id" : ontology_index[featureSchemaId]["encoded_value"],
-                "name" : ontology_index[featureSchemaId]['name'],
-                "keypoints" : keypoints,
-                "skeleton" : skeleton,
+                "supercategory" : ontology_schema_to_name_path[featureSchemaId]['name'],
+                "id" : ontology_schema_to_name_path[featureSchemaId]["encoded_value"],
+                "name" : ontology_schema_to_name_path[featureSchemaId]['name'],
+                "keypoints" : keypoints, "skeleton" : skeleton,
             })
-        elif ontology_index[featureSchemaId]["type"] == "point": 
+        elif ontology_schema_to_name_path[featureSchemaId]["type"] == "point": 
             categories.append({
-                "supercategory" : ontology_index[featureSchemaId]['name'],
-                "id" : ontology_index[featureSchemaId]["encoded_value"],
-                "name" : ontology_index[featureSchemaId]['name'],
-                "keypoints" : ['point'],
-                "skeleton" : [0, 0],
+                "supercategory" : ontology_schema_to_name_path[featureSchemaId]['name'],
+                "id" : ontology_schema_to_name_path[featureSchemaId]["encoded_value"],
+                "name" : ontology_schema_to_name_path[featureSchemaId]['name'],
+                "keypoints" : ['point'], "skeleton" : [0, 0],
             })        
-        elif ontology_index[featureSchemaId]['kind'] == 'tool':
+        elif ontology_schema_to_name_path[featureSchemaId]['kind'] == 'tool':
             categories.append({
-                "supercategory" : ontology_index[featureSchemaId]['name'],
-                "id" : ontology_index[featureSchemaId]["encoded_value"],
-                "name" : ontology_index[featureSchemaId]['name']
+                "supercategory" : ontology_schema_to_name_path[featureSchemaId]['name'],
+                "id" : ontology_schema_to_name_path[featureSchemaId]["encoded_value"],
+                "name" : ontology_schema_to_name_path[featureSchemaId]['name']
             })     
-        elif len(ontology_index[featureSchemaId]['parent_featureSchemaIds']) == 2:
-            supercategory = ontology_index[ontology_index[featureSchemaId]['parent_featureSchemaIds'][0]]['name']
+        elif len(ontology_schema_to_name_path[featureSchemaId]['name_path'].split(divider)) == 2:
+            supercategory = ontology_schema_to_name_path[ontology_schema_to_name_path[featureSchemaId]['parent_featureSchemaIds'][0]]['name']
             categories.append({
                 "supercategory" : supercategory,
-                "id" : ontology_index[featureSchemaId]["encoded_value"],
-                "name" : ontology_index[featureSchemaId]['name']
+                "id" : ontology_schema_to_name_path[featureSchemaId]["encoded_value"],
+                "name" : ontology_schema_to_name_path[featureSchemaId]['name']
             })
-    print(f'\nOntology Conversion Complete')       
-    coco_dataset = {
-        "info" : info,
-        "licenses" : licenses,
-        "images" : images,
-        "annotations" : annotations,
-        "categories" : categories
-    }      
-    print(f'\nCOCO Conversion Complete')    
-    return coco_dataset
+    if verbose:            
+        print(f'Ontology Conversion Complete')       
+    if verbose:            
+        print(f'COCO Conversion Complete')    
+    return {"info" : info, "licenses" : licenses, "images" : images, "annotations" : annotations, "categories" : categories}      
