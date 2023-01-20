5895648a2c9181196cfac96ce26b803f54fd4b7a
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-20 13:59:23 -0500

    Update __init__.py

--- labelbase/__init__.py
+++ labelbase/__init__.py
@@ -2,5 +2,5 @@ name = "labelbase"
 __version__ = "0.0.45"
 
 from labelbase.client import Client
+from labelbase import converters
 from labelbase.connector import *
-from labelbase.converters import *
