e0430661c903e4bbf98a9d644f67b12480a12c0c
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-01-25 16:52:35 -0500

    Update metadata.py

--- labelbase/metadata.py
+++ labelbase/metadata.py
@@ -83,16 +83,16 @@ def sync_metadata_fields(client:labelboxClient, table, get_columns_function, add
     # If your table doesn't have columns for all your metadata_field_names, make columns for them
     if type(table) != bool:
         if metadata_index:
-            column_names = get_columns_function(table, client=extra_client)
+            column_names = get_columns_function(table, extra_client=extra_client)
             for metadata_field_name in metadata_index.keys():
                 if metadata_field_name not in column_names:
-                    table = add_column_function(table, column_name=metadata_field_name, default_value=None, client=extra_client)
+                    table = add_column_function(table, column_name=metadata_field_name, default_value=None, extra_client=extra_client)
     # If Labelbox doesn't have metadata for all your metadata_field_names, make Labelbox metadata fields
     for metadata_field_name in metadata_index.keys():
         metadata_type = metadata_index[metadata_field_name]
         # Check to see if a metadata index input is a metadata field in Labelbox. If not, create the metadata field in Labelbox. 
         if metadata_field_name not in lb_metadata_names:
-            enum_options = get_unique_values_function(table, metadata_field_name, client=extra_client) if metadata_type == "enum" else []
+            enum_options = get_unique_values_function(table, metadata_field_name, extra_client=extra_client) if metadata_type == "enum" else []
             lb_mdo.create_schema(name=metadata_field_name, kind=conversion[metadata_type], options=enum_options)
             lb_mdo, lb_metadata_names = _refresh_metadata_ontology(client)
     if 'lb_integration_source' not in lb_metadata_names:
