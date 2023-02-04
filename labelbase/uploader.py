11e83f47b1494a1e13f8acb6d07487b5ca17908e
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-04 11:16:53 -0500

    Update uploader.py

--- labelbase/uploader.py
+++ labelbase/uploader.py
@@ -175,7 +175,7 @@ def batch_rows_to_project(client:labelboxClient, project_id_to_batch_dict:dict, 
     try:
         batch_number = 0
         for project_id in project_id_to_batch_dict:
-            project - client.get_project(project_id)
+            project = client.get_project(project_id)
             for i in range(0, len(project_id_to_batch_dict[project_id]), batch_size):
                 batch_number += 1
                 data_row_ids = project_id_to_batch_dict[project_id]
