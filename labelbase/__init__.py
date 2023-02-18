21bfac41e78fbcad4ad4e5d0b7396a4b7d3841f5
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-17 19:50:34 -0500

    Update __init__.py

--- labelbase/__init__.py
+++ labelbase/__init__.py
@@ -3,6 +3,7 @@ __version__ = "0.0.51"
 
 from labelbase import converters
 from labelbase import uploader
+from labelbase import downloader
 from labelbase import metadata
 from labelbase import ontology
 from labelbase import dataset
