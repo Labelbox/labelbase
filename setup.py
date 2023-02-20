2e6e629f2e92e17e095d4303a4e18b6b2fc89a78
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-20 15:28:54 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='labelbase',
-      version='0.0.68',
+      version='0.0.69',
       author='Labelbox',
       author_email='raphael@labelbox.com',
       description='Labelbox Helper Library',      
