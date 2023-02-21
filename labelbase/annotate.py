4d23e24b9cc4e1991cad74ab4d1ab968658616f2
raphaeljafriLB <79109817+raphaeljafriLB@users.noreply.github.com>
2023-02-20 21:32:54 -0500

    Update annotate.py

--- labelbase/annotate.py
+++ labelbase/annotate.py
@@ -151,6 +151,7 @@ def create_ndjsons(top_level_name:str, annotation_inputs:list, ontology_index:di
     """
     ndjsons = []
     print(annotation_inputs)    
+    print(type(annotation_inputs))
     if type(annotation_inputs) == str:
         if annotation_inputs:
             print(f'Converting string to list')
