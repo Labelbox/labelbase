49026e0ec6c6ed378541f7ce5004cc48cf1e2072
Luke Moehlenbrock <luksta@Lukes-MacBook-Pro.local>
2023-07-12 21:17:02 -0700

    fixed case sensitivity issue

--- labelbase/connector.py
+++ labelbase/connector.py
@@ -78,13 +78,13 @@ def validate_columns(client:labelboxClient, table, get_columns_function, get_uni
                 annotation_type, top_level_class_name = column_name.split(divider)[1:]
                 if annotation_type.lower() not in accepted_annotation_types:
                     raise ValueError(f"Invalid value in annotation column name {column_name} - must be `annotation{divider}` followed by one of the following: |{accepted_annotation_types}| followed by `{divider}top_level_feature_name`")
-                x["annotation_index"][column_name] = top_level_class_name.lower()                    
+                x["annotation_index"][column_name] = top_level_class_name                  
             # Prediction columns --> prediction///annotation_type///top_level_class_name
             elif input_type.lower() == "prediction":
                 annotation_type, top_level_class_name = column_name.split(divider)[1:]
                 if annotation_type.lower() not in accepted_annotation_types:
                     raise ValueError(f"Invalid value in prediction column name {column_name} - must be `prediction{divider}` followed by one of the following: |{accepted_annotation_types}| followed by `{divider}top_level_feature_name`")                    
-                x["prediction_index"][column_name] = top_level_class_name.lower()    
+                x["prediction_index"][column_name] = top_level_class_name   
             else:
                 continue
         else:
