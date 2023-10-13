02fee1825caa47ed5e3c16f754ace0b32a581488
Luke Moehlenbrock <luksta@Lukes-MacBook-Pro.local>
2023-10-13 15:12:55 -0700

    improved upload result message

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
       name='labelbase',
-      version='0.1.00',
+      version='0.1.06',
       author='Labelbox',
       author_email='raphael@labelbox.com',
       description='Labelbox Helper Library',      
